b2b32e618eef424f48fbe257ee801f39fc500e4c
ljgjy-pona <gjyhwbl@gmail.com>
2025-04-07 08:13:48 +0800

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -7,14 +7,15 @@ fetch('/videos.json')
     if (!videoData) throw new Error('视频不存在');
 
     // 生成Google Drive直连
-    const videoUrl = `https://drive.google.com/uc?export=download&id=${video.driveid}&confirm=yes`;
+    const driveUrl = `https://drive.google.com/uc?export=view&id=${videoData.driveid}`;
+    const videoUrl = `https://drive.google.com/uc?export=download&id=${videoData.driveid}&confirm=yes`;
 
     // 初始化播放器
     const player = videojs('myVideo', {
       controls: true,
       fluid: true,
       sources: [{
-        src: videoUrl,
+        src: driveUrl,
         type: 'video/mp4'
       }]
     });
